5909a5beff3ab31776427286c9a3e5d04504a3d8
leidaojun <1148166172@qq.com>
2019-06-09 20:37:52 +0800

    再次提交

--- src/cn/itcast/Test01.java
+++ src/cn/itcast/Test01.java
@@ -4,4 +4,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println(11111111);
     }
+    public void Test02(){
+        int a=10;
+        System.out.println(a);
+    }
 }
